96b36e67ae80d928f4b48114972f59fe14c0ad64
deetschoe <deetschoening@gmail.com>
2025-02-05 14:18:16 -0500

    d

--- next.config.ts
+++ next.config.ts
@@ -7,16 +7,22 @@ const nextConfig = {
         hostname: 'media.istockphoto.com',
       },
       {
+        protocol: 'https',
         hostname: 'i.ibb.co',
       },
       {
+        protocol: 'https',
         hostname: 'www.usatoday.com',
       },
       {
+        protocol: 'https',
         hostname: 'japannews.yomiuri.co.jp',
-      },
+      }
     ],
   },
+  // Add these to help with build issues
+  reactStrictMode: true,
+  swcMinify: true,
 }
 
 module.exports = nextConfig
