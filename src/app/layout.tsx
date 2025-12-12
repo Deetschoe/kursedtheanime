c0b4d02591505e4b578d227fca2662d6fc50c4dc
deetschoe <deetschoening@gmail.com>
2025-12-12 11:21:34 -0800

    Update Next.js and React

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,17 +28,15 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <head>
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
         <Script 
           defer
           data-domain="kursedtheanime.com" 
           src="https://plausible.io/js/script.js"
           strategy="afterInteractive"
         />
-      </head>
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
         {children}
       </body>
     </html>
